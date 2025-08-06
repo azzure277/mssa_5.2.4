13ff78714d35385ce0f96350a1b537e213f0da4c
azzure277 <brandon.govender21@outlook.com>
2025-08-05 19:45:34 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace mssa_5._2._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input a string : ");
+            string input = Console.ReadLine();
+
+            if (IsPalindrome(input, 0, input.Length - 1))
+            {
+                Console.WriteLine("The string is Palindrome.");
+            }
+            else
+            {
+                Console.WriteLine("The string is not Palindrome.");
+            }
+        }
+
+        static bool IsPalindrome(string str, int left, int right)
+        {
+            if (left >= right)
+                return true;
+
+            if (char.ToUpper(str[left]) != char.ToUpper(str[right]))
+                return false;
+
+            return IsPalindrome(str, left + 1, right - 1);
+        }
+    }
+}
